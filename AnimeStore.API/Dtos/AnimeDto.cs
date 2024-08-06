45c70fae19fe4654ac070af82a6744e1d19c3d7d
leoestevves <leogoncalves60@gmail.com>
2024-08-06 13:20:32 -0300

    Anime DTO / Get endpoint / Anime list Mock

---
+++ AnimeStore.API/Dtos/AnimeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimeStore.API.Dtos;
+
+public record class AnimeDto(
+    int Id,
+    string Name,
+    string Genre,
+    int NumberEpisodes,
+    DateOnly ReleaseDate
+);
--- AnimeStore.API/Program.cs
+++ AnimeStore.API/Program.cs
@@ -1,6 +1,39 @@
+using AnimeStore.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<AnimeDto> animes =
+[
+    new(
+        1,
+        "Solo Leveling",
+        "Fantasy",
+        13,
+        new DateOnly(2024, 01, 07)
+    ),
+    new(
+        2,
+        "Sousou no Frieren",
+        "Fantasy",
+        28,
+        new DateOnly(2023, 09, 29)
+    ),
+    new(
+        3,
+        "The Apothecary Diaries",
+        "Drama",
+        24,
+        new DateOnly(2023, 10, 22)
+    )
+];
+
+//GET /animes
+app.MapGet("animes", () => animes);
+
+//GET /animes/1
+app.MapGet("animes/{id}", (int id) => animes.Find(anime => anime.Id == id));
+
+
 
 app.Run();
