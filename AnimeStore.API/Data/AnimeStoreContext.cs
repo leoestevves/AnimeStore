2f1c932f82863ddbdf2dfb515616178580b4372b
Leonardo Esteves <leogoncalves60@gmail.com>
2024-08-15 12:43:59 -0300

    Seeding Data

--- AnimeStore.API/Data/AnimeStoreContext.cs
+++ AnimeStore.API/Data/AnimeStoreContext.cs
@@ -6,6 +6,17 @@ namespace AnimeStore.API.Data;
 public class AnimeStoreContext(DbContextOptions<AnimeStoreContext> options ) : DbContext(options)
 {
     public DbSet<AnimeEntity> Animes => Set<AnimeEntity>();
-
     public DbSet<GenreEntity> Genres => Set<GenreEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<GenreEntity>().HasData
+        (
+            new {Id = 1, Name = "Fantasy"},
+            new {Id = 2, Name = "Drama"},
+            new {Id = 3, Name = "Romance"},
+            new {Id = 4, Name = "Comedy"},
+            new {Id = 5, Name = "Action"}
+        );
+    }
 }
--- AnimeStore.API/Data/Migrations/20240815154007_SeedGenres.Designer.cs
+++ AnimeStore.API/Data/Migrations/20240815154007_SeedGenres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AnimeStore.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AnimeStore.API.Data.Migrations
 {
     [DbContext(typeof(AnimeStoreContext))]
-    partial class AnimeStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240815154007_SeedGenres")]
+    partial class SeedGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
